8fee9e82e6ac4c1798104473be16fdd022be4a81
mipopon <anchosanchos@gmail.com>
2023-04-30 18:33:09 +0200

    Day 10 | 110 Capitalize | typescript will infer head, tail from string

--- type-challenges/questions/medium/106.trim-left.ts
+++ type-challenges/questions/medium/106.trim-left.ts
@@ -19,7 +19,7 @@
 /* _____________ Your Code Here _____________ */
 
 type Space = ' ' | "\n" | "\t"
-type TrimLeft<S extends String> = S extends `${Space}${infer R}` ? TrimLeft<R> : S
+type TrimLeft<S extends string> = S extends `${Space}${infer R}` ? TrimLeft<R> : S
 
 /* _____________ Test Cases _____________ */
 import type { Equal, Expect } from '@type-challenges/utils'
