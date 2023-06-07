f20dbe9abbefafe5500a9958f86a735a31a0e9f6
Genli Nishioka <genli.nishioka@recharge.com>
2023-06-07 21:27:19 +0200

    Day 14 | 599 Merge | either use mapped types with double conditional or use intersection types and resolve with prettify

---
+++ type-challenges/questions/medium/599.merge.ts
@@ -0,0 +1,60 @@
+/*
+  599 - Merge
+  -------
+  by ZYSzys (@ZYSzys) #medium #object
+
+  ### Question
+
+  Merge two types into a new type. Keys of the second type overrides keys of the first type.
+
+  For example
+
+  ```ts
+  type foo = {
+    name: string;
+    age: string;
+  }
+  type coo = {
+    age: number;
+    sex: string
+  }
+
+  type Result = Merge<foo,coo>; // expected to be {name: string, age: number, sex: string}
+  ```
+
+  > View on GitHub: https://tsch.js.org/599
+*/
+
+/* _____________ Your Code Here _____________ */
+
+// basic answer always works
+// type Merge<F, S> = { [K in (keyof F | keyof S)]: K extends keyof S ? S[K] : F[K & keyof F] }
+
+type Merge<F, S> = Prettify<Omit<F, keyof S> & S>; // Equal function can't evaluate properly unless resolved with prettify
+
+/* _____________ Test Cases _____________ */
+import type { Equal, Expect, Prettify } from '@type-challenges/utils'
+
+type Foo = {
+  a: number
+  b: string
+}
+type Bar = {
+  b: number
+  c: boolean
+}
+
+type cases = [
+  Expect<Equal<Merge<Foo, Bar>, {
+    a: number
+    b: number
+    c: boolean
+  }>>,
+]
+
+/* _____________ Further Steps _____________ */
+/*
+  > Share your solutions: https://tsch.js.org/599/answer
+  > View solutions: https://tsch.js.org/599/solutions
+  > More Challenges: https://tsch.js.org
+*/
